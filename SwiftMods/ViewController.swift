23b63b88e68db09040bfb8ee5602bd0dea5afdd9
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2018-07-21 00:35:28 -0700

    Initial Commit

---
+++ SwiftMods/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftMods
+//
+//  Created by Cyril Garcia on 7/21/18.
+//  Copyright © 2018 Cyril Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
